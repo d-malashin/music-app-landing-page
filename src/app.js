83d2777ee16ee4634861c10f73632650dcf64702
denis-md <dmalashin@gmail.com>
2020-05-24 22:43:33 +0300

    images fix for webpack

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 import "./main.scss";
+
+// Images for webpack
+import "./assets/images/App-Store-Badge–US-Black.png";
+import "./assets/images/BlackBerry.png";
+import "./assets/images/facebook.png";
+import "./assets/images/Google-Play-Badge-US.png";
+import "./assets/images/instagram.png";
+import "./assets/images/iPhone-11-Pro.png";
+import "./assets/images/logo.png";
+import "./assets/images/playstation-wordmark.png";
+import "./assets/images/redbull.png";
+import "./assets/images/SENNHEISER_Basic_logo.png";
+import "./assets/images/Shape.png";
+import "./assets/images/tiktok-icon-white-1.png";
+import "./assets/images/twitter.png";
+import "./assets/images/promo-left/3af0e55ea66ea69e35145fb108b4a636.png";
+import "./assets/images/promo-left/3f9b1ab0095226f5af5f5720e1a49acd.png";
+import "./assets/images/promo-left/8ddf985207de08d839f8d6e0afebd762.png";
+import "./assets/images/promo-left/39e4598ce6d83898aa96468e9af8993e.png";
+import "./assets/images/promo-left/12545431_486880568181753_2073200248_n.png";
+import "./assets/images/promo-left/ffbee5009a7719cc608d1ba337388fdf.png";
+import "./assets/images/promo-right/Artboard@3x.png";
+import "./assets/images/promo-right/Artboard@3xa.png";
